5b0c89968ff34f072700d73373ade4d519d038c8
Forever Inc dev <120944776+foreverinc@users.noreply.github.com>
2023-07-06 09:06:29 +0300

    save

--- js/whatsapp.js
+++ js/whatsapp.js
@@ -1,9 +1,2 @@
 
-    document.getElementById("activate").addEventListener("click", function(event) {
-        event.preventDefault(); // Prevent the default behavior of the button
-        document.getElementById("chat-card").style.display = "block"; // Show the chat-card element
-    });
-document.getElementById("close").addEventListener("click", function () {
-        document.getElementById("chat-card").style.display = "none";
-    })
-
+    
